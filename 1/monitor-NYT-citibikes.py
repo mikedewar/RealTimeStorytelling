2361cab24593448fde6c870f0d62502ac71c42af
michael dewar <mikedewar@gmail.com>
2016-01-26 23:04:44 -0500

    monitoring all citibike stations

--- 1/monitor-NYT-citibikes.py
+++ 1/monitor-NYT-citibikes.py
@@ -2,20 +2,26 @@ import requests
 import json
 import time
 
-oldAvailableBikes = 0
+# this script polls the citibike API, looking for changes in the number of 
+# citibikes at each station
+
+availableBikes = {}
+
 while True:
     # get the citibike response from their API
     r = requests.get("https://www.citibikenyc.com/stations/json")
-    # extract my favourite citibike station
     for m in r.json()["stationBeanList"]:
-        if m["stationName"] == "W 41 St & 8 Ave":
-            availableBikes = m["availableBikes"]
-            print availableBikes
-            print m
-            if availableBikes > oldAvailableBikes:
-                print "{'new bikes returned':%s}"%availableBikes-oldAvailableBikes
-            if availableBikes < oldAvailableBikes:
-                print "{'bikes taken':%s}"%oldAvailableBikes-availableBikes
-            break
-    oldAvailableBikes = availableBikes
+        # for each station, initialise the store if necessary
+        if m["stationName"] not in availableBikes:
+            availableBikes[m["stationName"]] = m["availableBikes"]
+            continue
+
+        delta = m["availableBikes"] - availableBikes[m["stationName"]]
+
+        # if the number of bikes have changed, emit a message
+        if delta != 0:
+            print '{"station":%s,"bikeDelta":%s}'%(m["stationName"], delta)
+
+        # update the store with the new number of available bikes for that station
+        availableBikes[m["stationName"]] = m["availableBikes"]
     time.sleep(10)
