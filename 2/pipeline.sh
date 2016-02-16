59f2dce84fdc0416f3ceb2a2866b750d241aa62d
Mike Dewar <mikedewar@gmail.com>
2016-02-15 20:14:23 -0500

    Update pipeline.sh

--- 2/pipeline.sh
+++ 2/pipeline.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+
+# make sure redis is running!
+
+# run this to insert data into redis
 python poisson.py | python diff.py | python insert.py
+
+# run this to poll redis and calculate the moving average from time to time 
 python avg.py
