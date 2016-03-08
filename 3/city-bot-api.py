5de1c27f5940a07f477c1abe46aea3126e85dae9
Michael Dewar <michael.dewar@nytimes.com>
2016-03-08 15:30:24 -0500

    corrected bugs in city-bot api

--- 3/city-bot-api.py
+++ 3/city-bot-api.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 import redis
 import collections
 import json
@@ -29,12 +30,24 @@ def probability():
     city = request.args.get('city', '')
     ref = request.args.get('referrer', '')
     # get the distribution for the city
+    print city
     d = conn.hgetall(city)
     # get the count for the referrer
-    c = d[ref]
+    try:
+      c = d[ref]
+    except KeyError:
+      return json.dumps({
+        "city": city, 
+        "prob": 0,
+        "referrer": ref
+      })
     # get the normalising constant
-    z = sum([float(v) for v in d.values])
-    return float(d)/z
+    z = sum([float(v) for v in d.values()])
+    return json.dumps({
+      "city": city, 
+      "prob": float(c)/z,
+      "referrer": ref
+      })
 
     
 
